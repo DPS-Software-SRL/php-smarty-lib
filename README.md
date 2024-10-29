dff55c5fd6fa003fdfc5a2433e23052be8f97ded
marcoAS <mascarnatto@dpsonline.com.ar>
2024-10-29 09:27:35 -0300

    Avance y puesta a punto

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # php-smarty-lib
 Adaptación de Smarty para DPS
+
+
+Aprovecha si existen estas variables de $_ENV 
+* $_ENV['KINT_ENABLED'] y $_ENV['KINT_SHOW_DEBUGINFO_AL_PIE']
+* $_ENV['TEMPLATES_FOLDER']
--- src/Smartydps.php
+++ src/Smartydps.php
@@ -2,53 +2,72 @@
 
 namespace Dps;
 
+use Smarty;
 use Cekurte\Environment\Environment as env;
+use Kint\Renderer\RichRenderer;
 
+
+/**
+ * Adaptacion de Smarty a DPS
+ * 
+ * @requires $_ENV['KINT_ENABLED'] 
+ * @requires $_ENV['KINT_SHOW_DEBUGINFO_AL_PIE']
+ * @requires $_ENV['TEMPLATES_FOLDER']
+ */
 class Smartydps extends Smarty {
+
+    private $tplFormy     = 'formy.tpl';
+    private $tplGrilla    = 'abm.tpl';
+    private $tplContenido = 'contenido.tpl';
 
     public function __construct() {
         parent::__construct();
 
-        // $smarty = new SmartyDPS();
-        
-        require_once( __DIR__ . "/plugins/function.lastVersion.php" );
-        require_once( __DIR__ . "/plugins/function.botones.php" );
-        require_once( __DIR__ . "/plugins/function.gitBranch.php" );
-        
-        $this->setTemplateDir( "{$_ENV['PATH_ROOT']}/templates" );
+        // incluye a todos los archivos encontrados en la carpeta plugins
+        foreach( glob( __DIR__ . "/plugins/function.*.php" ) as $filePlugin ) {
+            require $filePlugin;
+        }
+
+        $this->setTemplateDir( env::get('TEMPLATES_FOLDER', "{$_ENV['PATH_ROOT']}/templates" ) );
         $this->setCompileDir( '/tmp' );
-        
-        // Cada Funcion PHP que se quiera usar dentro de los TPL debe primero ser registrada.
-        $this->registerPlugin( 'modifier', 'strstr', 'strstr' );
-        $this->registerPlugin( 'modifier', 'is_numeric', 'is_numeric' );
-        
-        $this->error_reporting = E_ALL & ~E_NOTICE & ~E_WARNING;
-        
-        // Esta linea setea un dato para UPD.TPL
-        if( isset( $_REQUEST['aceptar_y_nuevo'] ) ) {
-          $this->assign('aceptar_y_nuevo', true);
+
+        // Registrar plugins declarados en registrar.json
+        $json = json_decode( file_get_contents( __DIR__ . "/registrar.json" ) );
+        foreach( $json as $tipo => $valores ) {
+            foreach( $valores as $valor ) {
+                $this->registerPlugin( $tipo, $valor, $valor );
+            }
         }
-        
-        if( isset( $_REQUEST['forceReloadAfterUpd'] ) ) {
-          $this->assign('forceReloadAfterUpd', true);
-        }
-        
                 
     }
 
     public function mostrar( $template ) {
-        if( env::get('KINT_ENABLED') && env::get('KINT_SHOW_DEBUGINFO_AL_PIE')  ) {
-            $old = Kint\Renderer\RichRenderer::$folder;
-            Kint\Renderer\RichRenderer::$folder = true;
+        // Mostrar un DEBUG al pie si está configurado
+        if( env::get('KINT_ENABLED', false) && env::get('KINT_SHOW_DEBUGINFO_AL_PIE', false)  ) {
+            $old = RichRenderer::$folder;
+            RichRenderer::$folder = true;
             d( [$_REQUEST, $_SESSION, $_ENV] );
-            Kint\Renderer\RichRenderer::$folder = $old;
+            RichRenderer::$folder = $old;
         }
 
-        // $_SESSION['dpsListaTemplates'] = array('contenido.tpl');
+        // Estas lineas setean un dato para UPD.TPL
+        if( isset( $_REQUEST['aceptar_y_nuevo'] ) ) {
+            $this->assign('aceptar_y_nuevo', true);
+        }
+        
+        if( isset( $_REQUEST['forceReloadAfterUpd'] ) ) {
+            $this->assign('forceReloadAfterUpd', true);
+        }
+        
+        // Permitira cargar en head.tpl los css y js de Formy
+        $this->assign( 'libFormy', ( ( array_key_exists( 'libFormy', $this->getTemplateVars()) || $template == $this->tplFormy ) ) );
+        
+        // Permitira cargar en head.tpl los css y js de las grillas
+        $this->assign( 'libGrilla', ( ( array_key_exists( 'libGrilla', $this->getTemplateVars()) || $template == $this->tplGrilla  ) ) );
+
+        // Mostrar el template solicitado
         $this->assign( 'mostrarTemplate', $template );
-        $this->assign( 'libFormy', ( ( array_key_exists( 'libFormy', $this->getTemplateVars()) || $template == 'formy.tpl') ) );
-        $this->assign( 'libGrilla', ( ( array_key_exists( 'libGrilla', $this->getTemplateVars()) || $template == 'abm.tpl') ) );
-        $this->display( 'contenido.tpl' );
+        $this->display( $this->tplContenido );
     }
 
 }
